05690aab77fd7ad982b3808adb61e6ec1f2bb47e
Xertis <118364459+Xertis@users.noreply.github.com>
2024-08-29 12:50:39 +0300

    FIX

--- CaiDiscordBot.py
+++ CaiDiscordBot.py
@@ -1,99 +1,131 @@
 import discord
-import characterai
-from characterai import PyCAI
-from characterai import PyAsyncCAI
+import random
+from characterai import aiocai
 from discord.ext import commands
 import difflib
-import asyncio
 
-#---confing
+#conf
 
-CaiToken = 'TOKEN'
-DisToken = 'TOKEN'
-DisPrefix = 'PREFIX'
+DATA = {
+    "Allert's Server": {
+        "char": "CHAR",
+        "cai_chat": "CHAT",
+        "discord_chat": "DCHAT"
+    },
+    "Our Quite Place": {
+        "char": "CHAR",
+        "cai_chat": "CHAT",
+        "discord_chat": "DCHAT"
+    },
+    "The Phoenix": {
+        "char": "CHAR",
+        "cai_chat": "CHAT",
+        "discord_chat": "DCHAT"
+    },
+}
 
-#Dictionary with char characters bots, where keys are server names and values are char.
-All_char = {'Server1': 'CHAR1', 
-            'Server2': 'CHAR2'}
+DEFAULT = {"char": "CHAR",
+           "cai_chat": "CHAT"}
 
-#Default char, enabled for servers that are not in All_char
-Default_char = 'CHAR'
+DIS_TOKEN = "TOKEN"
+CAI_TOKEN = "TOKEN"
+DIS_PREFIX = ">>"
+TARGET_WORDS = ['A', 'B', 'C', 'A', 'B', 'C', 'A', 'B', 'C', 'A']
+MAX_MESSAGES = 15
 
-#A dictionary with Discord server names and their channels represented as IDs where the bot will communicate.
-chats_servers = {'Server1': 'chat_id1', 
-            'Server2': 'chat_id2'}
+##############################
 
-#The maximum packet size of messages, upon reaching which the bot will send messages to Characterai
-Max_message = 5
+client = aiocai.Client(CAI_TOKEN)
 
-#---end_config
-
-All_servers = {}
-intents = discord.Intents.default()
+intents = discord.Intents().all()
 intents.message_content = True
 
+All_servers = {}
+
+async def CheckMessage(text):
+    words_to_censor = set(TARGET_WORDS)
+    
+    async def is_banned(word):
+        for banned_word in words_to_censor:
+            if difflib.SequenceMatcher(None, word.lower(), banned_word.lower()).ratio() > 0.8:
+                return True
+        return False
+
+    censored_string = []
+    for word in text.split():
+        if await is_banned(word):
+            word_check = list(word)
+            word_check.pop(0)
+            random.shuffle(word_check)
+            censored_string.append(''.join(word_check))
+        else:
+            censored_string.append(word)
+
+    return ' '.join(censored_string)
+    
 #Cai Start
 async def Cai(message_user, server, user_name):
-    if message_user == '' or message_user == ' ':
-        message_user = 'None'
+    if message_user == '' and message_user == ' ':
+        message_user = "Image"
     try:
-        client = PyAsyncCAI(CaiToken)
-        if server in All_char.keys():
-            chat = await client.chat.get_chat(All_char[server])
+        message_user = await CheckMessage(message_user)
+        
+        char = ''
+        chat_id = ''
+        if server in DATA.keys():
+            char = DATA[server]["char"]
+            chat_id = DATA[server]["cai_chat"]
         else:
-            chat = await client.chat.get_chat(Default_char)
-
-        participants = chat['participants']
-        if not participants[0]['is_human']:
-            tgt = participants[0]['user']['username']
-        else:
-            tgt = participants[1]['user']['username']
-
+            char = DEFAULT["char"]
+            chat_id = DEFAULT["cai_chat"]
+        
+        text = ''
         if user_name != False:
-            message = '[' + user_name + ']' + ' ' + message_user
+            text = f"[{user_name}] {message_user}"
         else:
-            message = message_user
-        data = await client.chat.send_message(
-            chat['external_id'], tgt, message
-        )
+            text = message_user
 
-        text = data['replies'][0]['text']
-        out = (f"{text}")
+        async with await client.connect() as chat:
 
-        print(out, 'CAI RESULT')
+            message = await chat.send_message(
+                char, chat_id, text
+            )
+            out = message.text
+            print(out, 'CAI RESULT')
+            if '@' in out:
+                out = out.split('@')
+                out = '||@||'.join(out)
+            out = await CheckMessage(out)
+            return out
 
-        #spoiler @
-        if '@' in out:
-            out = out.split('@')
-            out = '||@||'.join(out)
-        return out
     except Exception as e:
         print(e)
 
 #Discord Start
-bot = commands.Bot(command_prefix=DisPrefix, intents = intents)
+bot = commands.Bot(command_prefix=DIS_PREFIX, intents = intents)
 
 async def pre_cai(ctx, server_name1, text1, MesID):
     ctx.message.id = MesID
-    print('pre_cai part')
+    print('pre_cai is ONLINE')
     await ctx.message.reply(await Cai(text1, server_name1, False))
 
 @bot.event
 async def on_message(message):
     global All_servers
+
     server_name = message.guild.name
     channel = message.channel.id
-    if server_name not in chats_servers.keys():
-        print(server_name, 'not in chats_servers')
+    if server_name not in DATA.keys():
+        print(server_name, 'не находится в списке серверов')
         await bot.process_commands(message)
     elif message.author != bot.user:
-        if message.content.startswith(DisPrefix):
+        if message.content.startswith(DIS_PREFIX):
             await bot.process_commands(message)
         else:
-            if chats_servers[server_name] == str(channel):
+            if DATA[server_name]["discord_chat"] == str(channel):
                 text = message.content
                 user_name = message.author
-                complete_message = '[' + str(user_name) + ']' + ' ' + text
+                complete_message = f"[{user_name}] {text}"
                 if server_name not in All_servers.keys():
                     All_servers[server_name] = complete_message
                 else:
@@ -101,7 +133,7 @@ async def on_message(message):
                     All_servers[server_name] = A_ssn + '\n' + complete_message
                 A_ssn2 = str(All_servers[server_name])
                 print(A_ssn2)
-                if A_ssn2.count('\n') >= Max_message:
+                if A_ssn2.count('\n') >= MAX_MESSAGES:
                     MesID = message.id
                     ctx = await bot.get_context(message)
                     del All_servers[server_name]
@@ -122,4 +154,4 @@ async def send(ctx, *, message):
     except Exception as e:
         print(e)
 
-bot.run(DisToken)
+bot.run(DIS_TOKEN)
